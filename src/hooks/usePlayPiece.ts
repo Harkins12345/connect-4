5be4eec21b85d7614a1cbe119e275b34187b48f2
zzzz57 <mbyzez@gmail.com>
2022-08-06 09:20:18 +0100

    Added diagonal win check

--- src/hooks/usePlayPiece.ts
+++ src/hooks/usePlayPiece.ts
@@ -4,6 +4,47 @@ import { boardState, gameOverState, playerState } from "state";
 
 const testWin = (arr: number[]): boolean => /1{4}|2{4}/.test(arr.join(""));
 
+/**
+  * Returns a diagonal array from if one is present in the game board
+  * @param xPos - X axis value of the last played piece
+  * @param yPos - Y axis value of the last played piece
+  * @param board - Nested number arrays making up the new board
+  * @param yAxisFlip - Set true (default) for the pointer to traverse up the board, false to traverse down
+  */
+const getDiagonalArray = (xPos: number, yPos: number, board: number[][], yAxisFlip: boolean = true): number[] => {
+  
+  // Set X axis pointer 3 places before the last played move
+  // Depending on which direction (up or down) you want the array to
+  // go "yAxisFlip" will be true or false (default true)
+
+  const pointer = {
+    x: xPos - 3,
+    y: yAxisFlip ? yPos - 3 : yPos + 3
+  }
+
+  const diagonalArray: number[] = []
+
+  // As only 4 need to connect to win
+  // Check -3 < last played position < + 3
+  for (let i: number = 0; i <= 6; i++){
+    // If the current coordinates are undefined they must be beyond the borders
+    // Or the move has not been made yet
+    if (board[pointer.x] &&
+        board[pointer.x][pointer.y]){
+
+          // Add valid position to diagonal array
+          diagonalArray.push(board[pointer.x][pointer.y])
+    }
+
+    // Move the pointer to next diagonal
+    // Up if "yAxisFlip" is true, down if false
+    pointer.y = yAxisFlip ? pointer.y + 1 : pointer.y - 1
+    pointer.x++
+  }
+  
+  return diagonalArray
+}
+
 const usePlayPiece = () => {
   const [board, setBoard] = useRecoilState(boardState);
   const [player, setPlayerTurn] = useRecoilState(playerState);
@@ -27,10 +68,12 @@ const usePlayPiece = () => {
 
     const row = newBoard[col].length - 1;
 
+
     if (
       testWin(newBoard[col]) || // Did win vertically
-      testWin(newBoard.map((col) => col[row] || 0)) // Did win horizontally
-      // TODO: Did win diagonally
+      testWin(newBoard.map((col) => col[row] || 0)) ||// Did win horizontally
+      testWin(getDiagonalArray(col, row, newBoard)) ||
+      testWin(getDiagonalArray(col, row, newBoard, false))// Did win diagonally
     ) {
       setGameOver(true);
     } else {
